1762832fdc5bec05d2bba0e38b5b51c7d569cdd2
alexs <alexstlopes@gmail.com>
2022-07-02 21:51:37 -0300

    122. Desenvolvendo a API de Contatos

---
+++ src/main/java/io/githube/alexstlopes/agendaapi/model/api/rest/ContatoController.java
@@ -0,0 +1,44 @@
+package io.githube.alexstlopes.agendaapi.model.api.rest;
+
+import io.githube.alexstlopes.agendaapi.model.entity.Contato;
+import io.githube.alexstlopes.agendaapi.model.reposotry.ContatoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/contatos")
+@RequiredArgsConstructor//Cria um construtor com argumentos obrigatórios
+public class ContatoController {
+
+    private final ContatoRepository repository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contato save(@RequestBody Contato contato){
+        return repository.save(contato);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Contato> list(){
+        return repository.findAll();
+    }
+
+    @PatchMapping("{id}/favorito")//Utilizado ao fazer atualização de uma propriedade em vez de todo o objeto,(seria o PUT)
+    public void favorite(@PathVariable Integer id, @RequestBody Boolean favorito){
+        Optional<Contato> contato = repository.findById(id);
+        contato.ifPresent( c -> {
+            c.setFavorito(favorito);
+            repository.save(c);
+        });
+    }
+}
